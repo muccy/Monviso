57340a2f3614b10b533d9a37008607862728c220
Muccy <muccymac@gmail.com>
2016-12-21 10:42:53 +0100

    Base data source protocol

--- Source/DataSource.swift
+++ Source/DataSource.swift
@@ -1,11 +1,8 @@
-import Foundation
-
-
-/// An object compliant to this protocol is able to provide content
-protocol DataSource {
+/// The ability to provide content
+public protocol DataSource {
     /// Type of content
     associatedtype Content
     
     /// Data source content
-    var content: Content? { get set }
+    var content: Content { get }
 }
