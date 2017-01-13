3a5e3cb6080c9c248bf0f54c7c6483aab7c93e7c
Muccy <muccymac@gmail.com>
2017-01-13 12:39:04 +0100

    Item update tests

--- Tests/Tests/Tests.swift
+++ Tests/Tests/Tests.swift
@@ -13,6 +13,26 @@ import Ferrara
 extension Int: Matchable {}
 extension String: Matchable {}
 
+struct Box<V: Equatable>: Matchable, Identifiable, Hashable {
+    let identifier: String
+    let value: V
+    
+    init(_ identifier: String, _ value: V) {
+        self.identifier = identifier
+        self.value = value
+    }
+    
+    var hashValue: Int {
+        return 4343 ^ identifier.hashValue
+    }
+    
+    public static func ==(lhs: Box, rhs: Box) -> Bool {
+        return lhs.identifier == rhs.identifier && lhs.value == rhs.value
+    }
+}
+
+// MARK: -
+
 class Tests: XCTestCase {
     func testDataSourceCreation() {
         let content = self.content(withSectionIdentifiers: ["a", "b"])
@@ -236,6 +256,263 @@ class Tests: XCTestCase {
         XCTAssertFalse(update.affectsItems)
     }
     
+    // MARK: - Item Updates
+    
+    func testItemInsertion() {
+        var content = self.content(withItems: ["a", "b"])
+        let update = content.update(sections: self.singleSection(withItems: ["a", "c", "b"]))
+        
+        XCTAssertEqual(update.insertedItems, indexPaths([1]))
+        XCTAssertEqual(update.deletedItems, indexPaths())
+        XCTAssertEqual(update.reloadedItems, indexPaths())
+        XCTAssertEqual(update.itemMovements, Set())
+        XCTAssertFalse(update.affectsSections)
+    }
+    
+    func testItemDeletion() {
+        var content = self.content(withItems: ["a", "b", "c"])
+        let update = content.update(sections: self.singleSection(withItems: ["a", "c"]))
+        
+        XCTAssertEqual(update.insertedItems, indexPaths())
+        XCTAssertEqual(update.deletedItems, indexPaths([1]))
+        XCTAssertEqual(update.reloadedItems, indexPaths())
+        XCTAssertEqual(update.itemMovements, Set())
+        XCTAssertFalse(update.affectsSections)
+    }
+    
+    func testItemReload() {
+        var content = self.content(withItems: ["a", Box("b", 0), "c"])
+        let update = content.update(sections: self.singleSection(withItems: ["a", Box("b", 1), "c"]))
+        
+        XCTAssertEqual(update.insertedItems, indexPaths())
+        XCTAssertEqual(update.deletedItems, indexPaths())
+        XCTAssertEqual(update.reloadedItems, indexPaths([1]))
+        XCTAssertEqual(update.itemMovements, Set())
+        XCTAssertFalse(update.affectsSections)
+    }
+    
+    func testItemReloadWithSectionMovement() {
+        var content = self.content(withSectionIdentifiers: ["a", "b"])
+        content.sections[1].items = ["a", Box("b", 0), "c"]
+        
+        var destinationSections = content.sections.reversed() as TableViewDataSource.Content.Sections
+        destinationSections[0].items[1] = Box("b", 1)
+        
+        let update = content.update(sections: destinationSections)
+        
+        XCTAssertEqual(update.insertedSections, IndexSet())
+        XCTAssertEqual(update.deletedSections, IndexSet())
+        XCTAssertEqual(update.reloadedSections, IndexSet())
+        XCTAssertEqual(update.sectionMovements, Set([ movement(0, 1) ]))
+        
+        XCTAssertEqual(update.insertedItems, indexPaths())
+        XCTAssertEqual(update.deletedItems, indexPaths())
+        XCTAssertEqual(update.reloadedItems, indexPaths([1]))
+        XCTAssertEqual(update.itemMovements, Set())
+    }
+    
+    func testItemMovement() {
+        var content = self.content(withItems: ["a", "b", "c", "d"])
+        let update = content.update(sections: self.singleSection(withItems: ["c", "a", "d", "b"]))
+        
+        XCTAssertEqual(update.insertedItems, indexPaths())
+        XCTAssertEqual(update.deletedItems, indexPaths())
+        XCTAssertEqual(update.reloadedItems, indexPaths())
+        XCTAssertEqual(update.itemMovements, Set([ movement(indexPath(0), indexPath(1)), movement(indexPath(1), indexPath(3)) ]))
+        XCTAssertFalse(update.affectsSections)
+    }
+    
+    func testItemMovementBetweenSections() {
+        var content = self.content(withSectionIdentifiers: ["a", "b"])
+        content.sections[0].items = ["a", "b"]
+        content.sections[1].items = ["c", "d", "e", "f"]
+        
+        var destinationSections = content.sections
+        destinationSections[0].items = ["c", "b", "f", "d"]
+        destinationSections[1].items = ["a", "e"]
+        
+        let update = content.update(sections: destinationSections)
+
+        XCTAssertEqual(update.insertedItems, indexPaths())
+        XCTAssertEqual(update.deletedItems, indexPaths())
+        XCTAssertEqual(update.reloadedItems, indexPaths())
+        // a: (0, 0) -> (1, 0)
+        // b: -
+        // c: (1, 0) -> (0, 0)
+        // d: (1, 1) -> (0, 3)
+        // e: -
+        // f: (1, 3) -> (0, 2)
+        XCTAssertEqual(update.itemMovements, Set([
+            movement(indexPath(0, 0), indexPath(1, 0)),
+            movement(indexPath(1, 0), indexPath(0, 0)),
+            movement(indexPath(1, 1), indexPath(0, 3)),
+            movement(indexPath(1, 3), indexPath(0, 2))
+        ]))
+        XCTAssertFalse(update.affectsSections)
+    }
+    
+    func testItemMovementToInsertedSection() {
+        // It's prohibited. Delete item from source section, instead.
+        
+        var content = self.content(withSectionIdentifiers: ["a"])
+        content.sections[0].items = ["a", "b"]
+        
+        var destinationSections = self.sections(withIdentifiers: ["a", "b"])
+        destinationSections[0].items = ["b"]
+        destinationSections[1].items = ["c", "a"]
+        
+        let update = content.update(sections: destinationSections)
+        
+        XCTAssertEqual(update.insertedSections, IndexSet(1...1))
+        XCTAssertEqual(update.deletedSections, IndexSet())
+        XCTAssertEqual(update.reloadedSections, IndexSet())
+        XCTAssertEqual(update.sectionMovements.count, 0)
+        
+        XCTAssertEqual(update.insertedItems, indexPaths())
+        XCTAssertEqual(update.deletedItems, indexPaths([0]))
+        XCTAssertEqual(update.reloadedItems, indexPaths())
+        XCTAssertEqual(update.itemMovements, Set())
+    }
+    
+    func testItemMovementFromDeletedSection() {
+        // It's prohibited. Insert item in destination section, instead.
+        
+        var content = self.content(withSectionIdentifiers: ["a", "b"])
+        content.sections[0].items = ["a", "b"]
+        content.sections[1].items = ["c"]
+        
+        var destinationSections = self.sections(withIdentifiers: ["b"])
+        destinationSections[0].items = ["c", "a"]
+        
+        let update = content.update(sections: destinationSections)
+        
+        XCTAssertEqual(update.insertedSections, IndexSet())
+        XCTAssertEqual(update.deletedSections, IndexSet(0...0))
+        XCTAssertEqual(update.reloadedSections, IndexSet())
+        XCTAssertEqual(update.sectionMovements.count, 0)
+        
+        XCTAssertEqual(update.insertedItems, indexPaths([1]))
+        XCTAssertEqual(update.deletedItems, indexPaths())
+        XCTAssertEqual(update.reloadedItems, indexPaths())
+        XCTAssertEqual(update.itemMovements, Set())
+    }
+    
+    func testItemInsertionDeletion() {
+        var content = self.content(withItems: ["a", "b"])
+        let update = content.update(sections: self.singleSection(withItems: ["c", "b"]))
+        
+        XCTAssertEqual(update.insertedItems, indexPaths([0]))
+        XCTAssertEqual(update.deletedItems, indexPaths([0]))
+        XCTAssertEqual(update.reloadedItems, indexPaths())
+        XCTAssertEqual(update.itemMovements, Set())
+        XCTAssertFalse(update.affectsSections)
+    }
+    
+    func testItemInsertionReload() {
+        var content = self.content(withItems: ["a", Box("b", 0)])
+        let update = content.update(sections: self.singleSection(withItems: ["c", "d", "a", "e", Box("b", 1), "f"]))
+        
+        XCTAssertEqual(update.insertedItems, indexPaths([0, 1, 3, 5]))
+        XCTAssertEqual(update.deletedItems, indexPaths())
+        XCTAssertEqual(update.reloadedItems, indexPaths([4]))
+        XCTAssertEqual(update.itemMovements, Set())
+        XCTAssertFalse(update.affectsSections)
+    }
+    
+    func testItemInsertionMovement() {
+        var content = self.content(withItems: ["a", "b", "c", "d"])
+        let update = content.update(sections: self.singleSection(withItems: ["e", "c", "b", "d", "f", "a"]))
+        
+        XCTAssertEqual(update.insertedItems, indexPaths([0, 4]))
+        XCTAssertEqual(update.deletedItems, indexPaths())
+        XCTAssertEqual(update.reloadedItems, indexPaths())
+        XCTAssertEqual(update.itemMovements, Set([
+            movement(indexPath(0), indexPath(5)),
+            movement(indexPath(1), indexPath(2)),
+            ]))
+        XCTAssertFalse(update.affectsSections)
+    }
+    
+    func testItemDeletionReload() {
+        var content = self.content(withItems: ["a", "b", "c", Box("d", 0)])
+        let update = content.update(sections: self.singleSection(withItems: ["b", Box("d", 1)]))
+        
+        XCTAssertEqual(update.insertedItems, indexPaths())
+        XCTAssertEqual(update.deletedItems, indexPaths([0, 2]))
+        XCTAssertEqual(update.reloadedItems, indexPaths([1]))
+        XCTAssertEqual(update.itemMovements, Set())
+        XCTAssertFalse(update.affectsSections)
+    }
+    
+    func testItemDeletionMovement() {
+        var content = self.content(withItems: ["a", "b", "c", "d", "e"])
+        let update = content.update(sections: self.singleSection(withItems: ["b", "e", "c"]))
+        
+        XCTAssertEqual(update.insertedItems, indexPaths())
+        XCTAssertEqual(update.deletedItems, indexPaths([0, 3]))
+        XCTAssertEqual(update.reloadedItems, indexPaths())
+        XCTAssertEqual(update.itemMovements, Set([ movement(indexPath(4), indexPath(1)) ]))
+        XCTAssertFalse(update.affectsSections)
+    }
+    
+    func testItemReloadMovement() {
+        var content = self.content(withItems: [Box("a", 0), "b", "c"])
+        let update = content.update(sections: self.singleSection(withItems: ["c", "b", Box("a", 1)]))
+        
+        XCTAssertEqual(update.insertedItems, indexPaths())
+        XCTAssertEqual(update.deletedItems, indexPaths())
+        XCTAssertEqual(update.reloadedItems, indexPaths([2]))
+        XCTAssertEqual(update.itemMovements, Set([
+            movement(indexPath(0), indexPath(2)),
+            movement(indexPath(2), indexPath(0))
+        ]))
+        XCTAssertFalse(update.affectsSections)
+    }
+    
+    func testItemInsertionDeletionReload() {
+        var content = self.content(withItems: ["a", Box("b", 0), "c"])
+        let update = content.update(sections: self.singleSection(withItems: ["a", "d", Box("b", 1), "e"]))
+        
+        XCTAssertEqual(update.insertedItems, indexPaths([1, 3]))
+        XCTAssertEqual(update.deletedItems, indexPaths([2]))
+        XCTAssertEqual(update.reloadedItems, indexPaths([2]))
+        XCTAssertEqual(update.itemMovements, Set())
+        XCTAssertFalse(update.affectsSections)
+    }
+    
+    func testItemInsertionDeletionMovement() {
+        var content = self.content(withItems: ["a", "b", "c"])
+        let update = content.update(sections: self.singleSection(withItems: ["b", "d", "a", "e"]))
+        
+        XCTAssertEqual(update.insertedItems, indexPaths([1, 3]))
+        XCTAssertEqual(update.deletedItems, indexPaths([2]))
+        XCTAssertEqual(update.reloadedItems, indexPaths())
+        XCTAssertEqual(update.itemMovements, Set([ movement(indexPath(0), indexPath(2)) ]))
+        XCTAssertFalse(update.affectsSections)
+    }
+    
+    func testItemDeletionReloadMovement() {
+        var content = self.content(withItems: [Box("a", 0), "b", "c", "d"])
+        let update = content.update(sections: self.singleSection(withItems: ["b", Box("a", 1), "d"]))
+        
+        XCTAssertEqual(update.insertedItems, indexPaths())
+        XCTAssertEqual(update.deletedItems, indexPaths([2]))
+        XCTAssertEqual(update.reloadedItems, indexPaths([1]))
+        XCTAssertEqual(update.itemMovements, Set([ movement(indexPath(0), indexPath(1)) ]))
+        XCTAssertFalse(update.affectsSections)
+    }
+    
+    func testItemInsertionDeletionReloadMovement() {
+        var content = self.content(withItems: [Box("a", 0), "b", "c"])
+        let update = content.update(sections: self.singleSection(withItems: ["b", "d", Box("a", 1), "e"]))
+        
+        XCTAssertEqual(update.insertedItems, indexPaths([1, 3]))
+        XCTAssertEqual(update.deletedItems, indexPaths([2]))
+        XCTAssertEqual(update.reloadedItems, indexPaths([2]))
+        XCTAssertEqual(update.itemMovements, Set([ movement(indexPath(0), indexPath(2)) ]))
+        XCTAssertFalse(update.affectsSections)
+    }
+    
     // MARK: -
     
     private func sections(withIdentifiers identifiers: [String]) -> TableViewDataSource.Content.Sections
@@ -250,10 +527,27 @@ class Tests: XCTestCase {
         return content
     }
     
+    private func singleSection(_ identifier: String = "a", withItems items: [Any]) -> TableViewDataSource.Content.Sections
+    {
+        return [ TableViewDataSource.Content.Element(identifier: identifier, items: items) ]
+    }
+    
     private func content(withItems items: [Any]) -> TableViewDataSource.Content {
         var content = TableViewDataSource.Content()
-        content.sections = [ TableViewDataSource.Content.Element(items: items) ]
+        content.sections = self.singleSection(withItems: items)
         return content
+    }
+    
+    private func indexPath(_ item: Int) -> IndexPath {
+        return IndexPath(item: item, section: 0)
+    }
+    
+    private func indexPath(_ section: Int, _ item: Int) -> IndexPath {
+        return IndexPath(item: item, section: section)
+    }
+    
+    private func indexPaths(_ items: [Int] = []) -> Set<IndexPath> {
+        return Set(items.map { indexPath($0) })
     }
     
     private func movement<L: Hashable>(_ from: L, _ to: L) -> SectionContainerUpdateMovement<L>
